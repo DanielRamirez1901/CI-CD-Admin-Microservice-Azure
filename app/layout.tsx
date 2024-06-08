1205380d684f608a9b343756846ad0829041e181
Dani Ramirez ~ <73015055+DanielRamirez1901@users.noreply.github.com>
2024-06-07 23:51:51 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dashboard Administrador',
+  title: 'Dashboard Administrador 2',
   description: 'E-Commerce Dashboard',
 }
 
